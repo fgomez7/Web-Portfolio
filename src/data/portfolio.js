e2a55e894b4b9174363e874754752dddce70e6cf
fgomez7 <fgomez7@ucsc.edu>
2024-04-15 20:23:18 -0700

    adding files

---
+++ src/data/portfolio.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'WebGl - BlockyFox',
+        imgUrl: '',
+        stack: ['WebGl', 'JavaScript'],
+        link: 'https://fgomez7.github.io/WebGl-Projects/ASGN2/BlockyFox.html',
+    },
+
+    {
+        title: 'Portfolio',
+        imgUrl: '',
+        stack:['React', 'Tailwind', 'Vite'],
+        link: 'https://fgomez7.github.io/Web-Portfolio/',
+    }, 
+
+    {
+        title: 'Ulog.gg',
+        imgUrl: '',
+        stack: ['Python', 'JavaScript', 'CSS'],
+        link: 'https://github.com/gabepontes/release',
+    }
+]
